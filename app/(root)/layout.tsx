f2e0f6ed572959d421687b51bec41fbd7fac0dd6
jahirulislam <jahirulislam.dev@gmail.com>
2025-01-06 23:43:14 +0600

    video call error

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,26 @@
 import { StreamVideoProvider } from "@/providers/StreamClientProvider";
 
-import React from "react";
+// import React from "react";
 
-function RootLayout({ children }: { children: React.ReactNode }) {
+// function RootLayout({ children }: { children: React.ReactNode }) {
+//   return (
+//     <main>
+//       <StreamVideoProvider>{children}</StreamVideoProvider>
+//     </main>
+//   );
+// }
+
+// export default RootLayout;
+import { ReactNode } from 'react';
+
+// import StreamVideoProvider from '@/providers/StreamClientProvider';
+
+const RootLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
   return (
     <main>
       <StreamVideoProvider>{children}</StreamVideoProvider>
     </main>
   );
-}
+};
 
 export default RootLayout;
